444070056fcdd41455689f6395666c9375691cbd
1MuRaD12 <tu71tfsws@code.edu.az>
2022-05-23 10:06:41 +0300

    first commit

---
+++ ap/ap/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ap.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult index(int id)
+        {
+            return Content(id.ToString());
+            return View();
+        }
+        public ActionResult About(int id)
+        {
+            ViewBag.praduc = "cola";
+            ViewData["Count"] = id;
+            TempData["Type"] = "cola";
+            return RedirectToAction("index");
+            return View();
+        }
+        public ActionResult contact()
+        {
+            return View();
+        }
+        public ActionResult FAQ(string name, string surname)
+        {
+            string Fullname = $"{name} {surname}";
+            return Content(Fullname);
+        }
+    }
+}
